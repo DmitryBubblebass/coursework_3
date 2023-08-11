27d50ab7736d538a440959b87061afdd800f936b
Dmitry Bubblebass <dmitry.bubblebass@gmail.com>
2023-08-11 12:19:26 +0300

    wrote main edit utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
 import json
 
 
-def get_data():
-    with open('operations.json', 'r', encoding='utf-8') as file:
+def get_data(data):
+    with open(data, 'r', encoding='utf-8') as file:
         data = json.load(file)
 
     return data
@@ -14,12 +14,12 @@ def get_executed_data(data):
 
 
 def get_last_values(data, count_l_v):
-    data = sorted(data, key=lambda x: x['data'], reverse=True)
+    data = sorted(data, key=lambda x: x['date'], reverse=True)
     data = data[:count_l_v]
     return data
 
 
-def get_format_data(data):
+def get_format_date(data):
     new_data = data[0:10].split('-')
     return new_data[2] + '.' + new_data[1] + '.' + new_data[0]
 
@@ -38,25 +38,38 @@ def get_masked_billnumber(billnumber):
         return 'Номер счета не валиден'
 
 
-def get_masked_massage(massage):
-    msg_split = massage.split(' ')
-    if msg_split[0] == 'Счет':
-        number_hidden = get_masked_billnumber(msg_split[1])
+def get_masked_message(message):
+    msg_split = message.split(' ')
+    # if msg_split[0] == 'Счет':
+    #     number_hidden = get_masked_billnumber(msg_split[1])
+    if msg_split[1].isdigit() is True:
+        if len(msg_split[1]) == 20:
+            number_hidden = get_masked_billnumber(msg_split[1])
+        elif len(msg_split[1]) == 16:
+            number_hidden = get_masked_cardnumber(msg_split[1])
+        final_message = msg_split[0] + ' ' + number_hidden
+        return final_message
+
     else:
-        number_hidden = get_masked_cardnumber(msg_split[1])
-    return number_hidden
+        if len(msg_split[2]) == 20:
+            number_hidden = get_masked_billnumber(msg_split[2])
+        elif len(msg_split[2]) == 16:
+            number_hidden = get_masked_cardnumber(msg_split[2])
+        final_message = msg_split[0] + ' ' + msg_split[1] + ' ' + number_hidden
+        return final_message
 
 
-# def prepare_user_msg(item):
-#     date = get_date(item.get('date'))
-#     desc = item.get('description')
-#     from_ = mask_from_to_msg(item.get('from'))
-#     to_ = mask_from_to_msg(item.get('to'))
-#     amount = item.get('operationAmount').get('amount')
-#     currency = item.get('operationAmount').get('currency').get('name')
-#
-#     # Переменная from_ может быть пустая, тогда '->' не надо. Если не пусто, то надо
-#     if from_:
-#         from_ = from_ + ' -> '
-#
-#     return f'{date} {desc}\n{from_}{to_}\n{amount} {currency}'
+def prepare_user_msg(item):
+    date = get_format_date(item.get('date'))
+    desc = item.get('description')
+
+    if 'from' in item:
+        from_ = get_masked_message(item.get('from'))
+    else:
+        from_ = ''
+    to_ = get_masked_message(item.get('to'))
+    amount = item.get('operationAmount').get('amount')
+    currency = item.get('operationAmount').get('currency').get('name')
+
+
+    return f'{date} {desc}\n{from_} -> {to_}\n{amount} {currency}\n'
